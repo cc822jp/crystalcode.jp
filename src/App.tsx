8ced9aff3fea8d200ecb940574bfb395b812ad8a
Tatsuya Sasaki <sasaki@crystalcode.jp>
2019-11-22 19:22:17 +0900

    Update: index.html

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,37 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        <section>
+          <h1></h1>
+        </section>
+        <ul>
+          <li>佐々木 達哉(Tatusya Sasaki)</li>
+          <li>東京都世田谷区</li>
+          <li>Web Developer</li>
+          <li>設立：2013/05/07</li>
+        </ul>
+    
+        <section>
+          <h1></h1>
+          
+          <ul>
+            <li>
+              <a href="mailto:sasaki@crystalcode.jp">sasaki@crystalcode.jp</a>
+            </li>
+            <li>
+              <a href="https://github.com/cc822jp">@cc822jp</a>
+            </li>
+            <li>
+              <a href="https://medium.com/@cc822jp" target="_blank">Blog</a>
+            </li>
+          </ul>
+        </section>
+      </main>
+      <footer>
+        <p>Copyright (C) CrystalCode All Rights Reserved.</p>
+      </footer>
     </div>
   );
 }
